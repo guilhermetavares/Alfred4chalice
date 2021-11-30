3ed980d7bbe44fa1a40fc91ed219d440f50e794c
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-11-30 09:54:44 -0300

    Add auth tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,5 @@
-import pytest
-
-from alfred.dynamodb_create_table import dynamodb_create_tables
+from alfred.dynamodb_create_tables import dynamodb_create_tables
 
 
 def pytest_configure(config):
     dynamodb_create_tables()
-
-
-@pytest.fixture(scope="module")
-def hash_password_1234():
-    return "201f5fd49dfdd09076a1dbb9651d9c592d64e999f3d1f09ffb099542e127cf7ed0a1cdee577648cf0f1ae34d1027e8eec33fea36b179cf655600175ca4d6d877"  # noqa E501
