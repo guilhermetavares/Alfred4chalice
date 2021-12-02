1648fe3b243660d804711a8df7dd472baabd45a9
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-12-02 18:12:05 -0300

    Add jwt_auhtorizer

--- alfred/auth/__init__.py
+++ alfred/auth/__init__.py
@@ -1,4 +1,12 @@
-from .authorizers import basic_auth_authorizer
+from .authorizers import basic_auth_authorizer, jwt_authorizer
 from .models import BasicAuthUser
+from .utils import JWTException, decode_auth, encode_auth
 
-__all__ = ["basic_auth_authorizer", "BasicAuthUser"]
+__all__ = [
+    "basic_auth_authorizer",
+    "BasicAuthUser",
+    "decode_auth",
+    "encode_auth",
+    "jwt_authorizer",
+    "JWTException",
+]
--- alfred/auth/authorizers.py
+++ alfred/auth/authorizers.py
@@ -1,14 +1,8 @@
-import base64
-
+import jwt
 from chalice import AuthResponse
 
 from .models import BasicAuthUser
-
-
-def get_credentials(auth64):
-    decoded = base64.b64decode(auth64).decode("utf-8")
-    username, password = decoded.split(":")
-    return username, password
+from .utils import decode_auth, get_credentials
 
 
 def basic_auth_authorizer(auth_request):
@@ -18,3 +12,15 @@ def basic_auth_authorizer(auth_request):
     routes = BasicAuthUser.login(username, password)
 
     return AuthResponse(routes=routes, principal_id=username)
+
+
+def jwt_authorizer(auth_request):
+    token = auth_request.token.replace("Token ", "")
+    try:
+        payload = decode_auth(token)
+    except (jwt.ExpiredSignatureError, jwt.DecodeError):
+        return AuthResponse(routes=[], principal_id="")
+
+    return AuthResponse(
+        routes=["*"], principal_id=payload["id"], context={"token": payload["token"]},
+    )
