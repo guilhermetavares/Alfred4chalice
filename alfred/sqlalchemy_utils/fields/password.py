5345896a080cf2f1271178e24ca830327dc170f5
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-12-07 11:12:32 -0300

    Add PasswordSaltType

--- alfred/sqlalchemy_utils/fields/password.py
+++ alfred/sqlalchemy_utils/fields/password.py
@@ -1,16 +1,17 @@
 import hashlib
+import uuid
 
 from sqlalchemy import types
 
 from alfred.settings import ALFRED_PASSWORD_SALT
 
 
-def hash_password(raw_password):
+def hash_password(raw_password, salt=None):
     value_encode = raw_password.encode()
 
-    password = hashlib.pbkdf2_hmac(
-        "sha512", value_encode, ALFRED_PASSWORD_SALT.encode(), 100000
-    ).hex()
+    salt_encode = salt.encode() if salt else ALFRED_PASSWORD_SALT.encode()
+
+    password = hashlib.pbkdf2_hmac("sha512", value_encode, salt_encode, 100000).hex()
     return password
 
 
@@ -24,6 +25,26 @@ class PasswordType(types.TypeDecorator):
             return None
 
         return password.encode()
+
+    def process_result_value(self, value, dialect):
+        try:
+            return value.decode()
+        except AttributeError:
+            return None
+
+
+class PasswordSaltType(types.TypeDecorator):
+    impl = types.VARBINARY(255)
+
+    def process_bind_param(self, value, dialect):
+        try:
+            salt = uuid.uuid4().hex
+            password = hash_password(value, salt)
+        except AttributeError:
+            return None
+
+        foo = f"{salt}-{password}"
+        return foo.encode()
 
     def process_result_value(self, value, dialect):
         try:
