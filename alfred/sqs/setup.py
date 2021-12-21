6701372280f2aa7b6428a81f00e70f457111cd4e
IsabelaToso <isabela.toso@gmail.com>
2021-12-21 11:13:54 -0300

    Adicionando arquivo setup.py

---
+++ alfred/sqs/setup.py
@@ -0,0 +1,42 @@
+import logging
+
+from alfred.settings import AWS
+from alfred.sqs import SQSHandler
+
+logger = logging.getLogger("base")
+
+
+def handle_sqs_message(event):
+    for record in event:
+        handler = SQSHandler(body=record.body)
+        try:
+            response = handler.apply()
+        except Exception as err:
+            logger.debug(
+                {
+                    "task_has_succeeded": False,
+                    "task_error_message": str(err),
+                    "task_function_module": handler.body["_func_module"],
+                    "task_function_name": handler.body["_func_name"],
+                    "task_function_args": handler.body["args"],
+                    "task_function_kwargs": handler.body["kwargs"],
+                    "task_function_retries": handler.body["retries"],
+                    "task_response": None,
+                }
+            )
+            raise err
+        else:
+            logger.debug(
+                {
+                    "task_has_succeeded": True,
+                    "task_error_message": None,
+                    "task_function_module": handler.body["_func_module"],
+                    "task_function_name": handler.body["_func_name"],
+                    "task_function_args": handler.body["args"],
+                    "task_function_kwargs": handler.body["kwargs"],
+                    "task_function_retries": handler.body["retries"],
+                    "task_response": response,
+                }
+            )
+        finally:
+            handler.sqs_delete_message(AWS["sqs"]["queue_url"], record.receipt_handle)
