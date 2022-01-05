2f3814e96a4bec5a15b675349be98e228c45709d
IsabelaToso <isabela.toso@gmail.com>
2022-01-05 11:52:42 -0300

    Testes ImageType

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from botocore.stub import Stubber
 
+from alfred.aws.s3 import s3_client
 from alfred.dynamodb_create_tables import dynamodb_create_tables
 from alfred.sqs.sqs import sqs_client
 
@@ -12,5 +13,12 @@ def pytest_configure(config):
 @pytest.fixture(autouse=True)
 def sqs_stub():
     with Stubber(sqs_client) as stubber:
+        yield stubber
+        stubber.assert_no_pending_responses()
+
+
+@pytest.fixture(autouse=True)
+def s3_stub():
+    with Stubber(s3_client) as stubber:
         yield stubber
         stubber.assert_no_pending_responses()
---
+++ tests/sqlalchemy_utils/test_image_type.py
@@ -0,0 +1,46 @@
+import pytest
+from sqlalchemy import types
+
+from alfred.aws import S3UploadFileException
+from alfred.sqlalchemy_utils.fields import ImageType
+
+
+def test_image_type_as_type_decorator():
+    assert issubclass(ImageType, types.TypeDecorator) is True
+
+
+def test_image_type_impl():
+    assert type(ImageType.impl) is types.Unicode
+    assert ImageType.impl.length == 128
+
+
+def test_image_type_process_bind_param_sucess(s3_stub):
+    s3_stub.add_response(
+        "put_object", service_response={},
+    )
+
+    file_base64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwC"
+    "AAAAC0lEQVR42mNkWAsAALMAr6o4KHcAAAAASUVORK5CYII="
+
+    t = ImageType(upload_to="avatars")
+    file_name = t.process_bind_param(value=file_base64)
+
+    assert file_name is not None
+
+
+def test_image_type_process_bind_param_s3_upload_file_exception():
+    file_base64 = "some_fake_base_64"
+
+    t = ImageType(upload_to="avatars")
+
+    with pytest.raises(S3UploadFileException):
+        t.process_bind_param(value=file_base64)
+
+
+def test_image_type_process_result_value_sucess():
+    file_name = "avatars/110fdf29-650f-480b-a01d-29b8f4788cb6.png"
+
+    t = ImageType(upload_to="avatars")
+    url = t.process_result_value(value=file_name)
+
+    assert url is not None
