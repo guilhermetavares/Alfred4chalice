1fb0138f2c30f8ce5b4624180928226858408bab
isabela.siqueira <isabela.siqueira@maistodos.com.br>
2022-01-24 18:26:02 -0300

    add field BRDocument para validar CPF via pycpfcnpj

--- alfred/marshmallow_utils/fields/document.py
+++ alfred/marshmallow_utils/fields/document.py
@@ -1,18 +1,18 @@
 from marshmallow import ValidationError, fields
 
-from validate_docbr import CPF
+from pycpfcnpj import cpf
 from alfred.tools import only_digits
 
 class BRDocumentField(fields.String):
     def _deserialize(self, value, attr, data, **kwargs):
         value = super()._deserialize(value, attr, data, **kwargs)
-        cpf = only_digits(value)
-        valid_CPF = CPF().validate(value)
+        document = only_digits(value)
+        valid_CPF = cpf.validate(value)
 
         if not valid_CPF:
             raise ValidationError(self.error_messages["document_error_msg"])
 
-        return cpf
+        return document
 
     def __init__(self, document_error_msg="CPF inválido"):
         super().__init__()
