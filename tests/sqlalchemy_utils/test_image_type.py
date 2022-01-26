92e43b873575cf66b1e406baff1f72d4cd632e29
IsabelaToso <isabela.toso@gmail.com>
2022-01-26 16:29:27 -0300

    Adicionando dialect ao sqlalchemy_utils.fields.ImageType

--- tests/sqlalchemy_utils/test_image_type.py
+++ tests/sqlalchemy_utils/test_image_type.py
@@ -23,7 +23,7 @@ def test_image_type_process_bind_param_sucess(s3_stub):
     "AAAAC0lEQVR42mNkWAsAALMAr6o4KHcAAAAASUVORK5CYII="
 
     t = ImageType(upload_to="avatars")
-    file_name = t.process_bind_param(value=file_base64)
+    file_name = t.process_bind_param(value=file_base64, dialect="object")
 
     assert file_name is not None
 
@@ -34,13 +34,13 @@ def test_image_type_process_bind_param_s3_upload_file_exception():
     t = ImageType(upload_to="avatars")
 
     with pytest.raises(S3UploadFileException):
-        t.process_bind_param(value=file_base64)
+        t.process_bind_param(value=file_base64, dialect="object")
 
 
 def test_image_type_process_result_value_sucess():
     file_name = "avatars/110fdf29-650f-480b-a01d-29b8f4788cb6.png"
 
     t = ImageType(upload_to="avatars")
-    url = t.process_result_value(value=file_name)
+    url = t.process_result_value(value=file_name, dialect="object")
 
     assert url is not None
