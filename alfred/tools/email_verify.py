fe44a3bbcd9c52658b6ad7fac045bede87c6d0d4
Isabela Nascimento <iaman.isabela@gmail.com>
2022-03-10 17:03:26 -0300

    Alterando classe e testes

--- alfred/tools/email_verify.py
+++ alfred/tools/email_verify.py
@@ -1,4 +1,6 @@
 import requests
+from alfred.settings import ALFRED_EMAIL_VERIFY_TOKEN
+
 
 APPROVE_LIST = [
     "ok",
@@ -8,20 +10,19 @@ APPROVE_LIST = [
 ]
 
 class EmailListVerifyOne():
-    def __init__(self, key, email):
-        self.key = key
-        self.email = email
+    def __init__(self):
+        self.key = ALFRED_EMAIL_VERIFY_TOKEN
         self.base_url = "https://apps.emaillistverify.com/api/verifyEmail?secret="
-        self.url = self.base_url+self.key+"&email="+self.email
+        self.url = self.base_url+self.key+"&email="
 
-    def control(self):
-        response = requests.get(self.url)
+    def control(self, email):
+        response = requests.get(self.url+email)
         return response.text
 
-    def verify(self):
+    def verify(self, email):
         try:
-            control = self.control()
+            control = self.control(email)
         except:
             return True
-        
+
         return True if control in APPROVE_LIST else False
--- tests/tools/test_email_verify.py
+++ tests/tools/test_email_verify.py
@@ -7,25 +7,22 @@ from alfred.tools.email_verify import EmailListVerifyOne
 
 @pytest.mark.vcr
 def test_email_valid_success():
-    key = "fake_key"
-    email = "valid.email@maistodos.com.br"
-    client = EmailListVerifyOne(key, email)
-    assert client.verify() is True
+    email = "contato@maistodos.com.br"
+    client = EmailListVerifyOne()
+    assert client.verify(email) is True
 
 
 @pytest.mark.vcr
 def test_email_invalid_success():
-    key = "fake_key"
     email = "invalid.email@maistodos.com.br"
-    client = EmailListVerifyOne(key, email)
-    assert client.verify() is False
+    client = EmailListVerifyOne()
+    assert client.verify(email) is False
 
 
 @pytest.mark.parametrize("err", (Timeout, ConnectTimeout, ReadTimeout, KeyError, TypeError))
 @patch("alfred.tools.email_verify.requests.get")
 def test_email_valid_error(mock_get, err):
     mock_get.side_effect = err
-    key = "fake_key"
     email = "valid.email@maistodos.com.br"
-    client = EmailListVerifyOne(key, email)
-    assert client.verify() is True
+    client = EmailListVerifyOne()
+    assert client.verify(email) is True
