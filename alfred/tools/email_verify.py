bb83993a9fc2bd160e1226556299eaf55ac8a8c8
Guilherme Tavares <zetavares.rib@gmail.com>
2022-03-13 15:34:41 -0300

    fix

--- alfred/tools/email_verify.py
+++ alfred/tools/email_verify.py
@@ -1,6 +1,6 @@
 import requests
-from alfred.settings import ALFRED_EMAIL_VERIFY_TOKEN
 
+from alfred.settings import ALFRED_EMAIL_VERIFY_TOKEN
 
 APPROVE_LIST = [
     "ok",
@@ -9,20 +9,21 @@ APPROVE_LIST = [
     "missing parameter",
 ]
 
-class EmailListVerifyOne():
-    def __init__(self):
-        self.key = ALFRED_EMAIL_VERIFY_TOKEN
-        self.base_url = "https://apps.emaillistverify.com/api/verifyEmail?secret="
-        self.url = self.base_url+self.key+"&email="
+
+class EmailListVerifyOne:
+    BASE_URL = (
+        "https://apps.emaillistverify.com/api/verifyEmail?secret={secret}&email={email}"
+    )
 
     def control(self, email):
-        response = requests.get(self.url+email)
+        url_ = self.BASE_URL.format(email=email, secret=ALFRED_EMAIL_VERIFY_TOKEN)
+        response = requests.get(url_)
         return response.text
 
     def verify(self, email):
         try:
             control = self.control(email)
-        except:
+        except Exception:
             return True
 
         return True if control in APPROVE_LIST else False
