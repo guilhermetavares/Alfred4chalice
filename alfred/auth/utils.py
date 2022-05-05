db1f3056f0d2a02c5eb42a8ff637483ed68b4a1d
Gabriela Rodrigues <gabrielarodrigues432@gmail.com>
2022-05-05 09:01:11 -0300

    adicionando ignore no decode

--- alfred/auth/utils.py
+++ alfred/auth/utils.py
@@ -17,7 +17,7 @@ from .exceptions import JWTException
 
 def get_credentials(auth64):
     try:
-        decoded = base64.b64decode(auth64).decode("utf-8")
+        decoded = base64.b64decode(auth64).decode("utf-8", "ignore")
         username, password = decoded.split(":")
     except binascii.Error:
         username = None
