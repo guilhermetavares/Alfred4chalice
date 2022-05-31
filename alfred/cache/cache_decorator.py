23edff74b16c6a5731ef11d1dbb42433f9a76e82
russianbb <bruno.russian.i@gmail.com>
2022-05-31 17:01:42 -0400

    Adiciona parametro is_classmethod ao cache_memoize

--- alfred/cache/cache_decorator.py
+++ alfred/cache/cache_decorator.py
@@ -4,12 +4,13 @@ from hashlib import md5
 from alfred.cache.walrus_cache import Cache
 
 
-def cache_memoize(cache_time=60 * 60):
+def cache_memoize(cache_time=60 * 60, is_classmethod=False):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
 
-            cache_key = f"{func.__module__}.{func.__name__}:{args}_{kwargs}"
+            clean_args = args[1:] if is_classmethod else args
+            cache_key = f"{func.__module__}.{func.__qualname__}:{clean_args}_{kwargs}"
             hash_key = md5(cache_key.encode()).hexdigest()
 
             cache = Cache()
