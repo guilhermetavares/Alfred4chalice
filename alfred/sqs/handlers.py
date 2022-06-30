165c114902a46798ed8324841a3019eea39911e4
Isabela Nascimento <iaman.isabela@gmail.com>
2022-06-30 13:05:19 -0300

    Alterando lógica de handler para receber id de execução de task

--- alfred/sqs/handlers.py
+++ alfred/sqs/handlers.py
@@ -8,16 +8,32 @@ class BaseHandler:
     def __init__(self, body):
         self.body = json.loads(body)
 
-    def apply(self):
+    def get_sqs_task(self):
         module = importlib.import_module(self.body["_func_module"])
-        sqs_task = getattr(module, self.body["_func_name"])
+        self.sqs_task = getattr(module, self.body["_func_name"])
+        self.config_sqs_task()
+        
+    def config_sqs_task(self):
+        pass  # pragma: no cover
 
-        return sqs_task._run(
+    def apply(self):
+        self.get_sqs_task()
+
+        return self.sqs_task._run(
             self.body["retries"], *self.body["args"], **self.body["kwargs"]
         )
 
 
 class SQSHandler(BaseHandler):
+    def __init__(self, body, aws_request_id=None, message_id=None):
+        super().__init__(body)
+        self.aws_request_id = aws_request_id
+        self.message_id = message_id
+    
+    def config_sqs_task(self):
+        self.sqs_task.message_id = self.message_id
+        self.sqs_task.aws_request_id = self.aws_request_id
+
     def sqs_delete_message(self, queue_url, receipt_handle):
         sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
 
--- alfred/sqs/setup.py
+++ alfred/sqs/setup.py
@@ -9,12 +9,21 @@ logger = logging.getLogger("base")
 def handle_sqs_message(event, queue_url=None):
     queue_url = queue_url or SQS_QUEUE_URL
     for record in event:
-        handler = SQSHandler(body=record.body)
+        record_dict = record.to_dict()
+
+        aws_request_id = record.context.aws_request_id
+        body = record_dict["body"]
+        message_id = record_dict["messageId"]
+
+        handler = SQSHandler(body, aws_request_id, message_id)
+        
         try:
             response = handler.apply()
         except Exception as err:
             logger.debug(
                 {
+                    "sqs_message_id": message_id,
+                    "aws_request_id": aws_request_id,
                     "task_has_succeeded": False,
                     "task_error_message": str(err),
                     "task_function_module": handler.body["_func_module"],
@@ -29,6 +38,8 @@ def handle_sqs_message(event, queue_url=None):
         else:
             logger.debug(
                 {
+                    "sqs_message_id": message_id,
+                    "aws_request_id": aws_request_id,
                     "task_has_succeeded": True,
                     "task_error_message": None,
                     "task_function_module": handler.body["_func_module"],
