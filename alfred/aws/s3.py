a762d5285e108eb32b68c27d0746ec7fd3a90e92
russianbb <bruno.russian.i@gmail.com>
2022-08-09 15:21:37 -0400

    Adiciona credenciais ao client do s3

--- alfred/aws/s3.py
+++ alfred/aws/s3.py
@@ -4,9 +4,13 @@ import uuid
 import boto3
 from botocore.exceptions import ClientError
 
-from alfred.settings import BUCKET_S3
+from alfred.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, BUCKET_S3
 
-s3_client = boto3.client("s3")
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
 
 
 class S3File:
